faffa71c664b4a768bd38a91d1ef333c561c1e36
SwapKam <swapxn@gmail.com>
2022-01-17 20:15:08 +0530

    Commit

---
+++ app/src/main/java/com/swapkam/mysql/URLs.java
@@ -0,0 +1,7 @@
+package com.swapkam.mysql;
+
+public class URLs {
+    private static final String ROOT_URL = "http://10.0.2.2/az-database/registrationapi.php?apicall=";
+    public static final String URL_REGISTER = ROOT_URL + "signup";
+    public static final String URL_LOGIN= ROOT_URL + "login";
+}
